4a8ab0c844f404a829387c44d76ad4dae3fb0b8c
derekque <quehan11@gmail.com>
2018-06-22 17:18:35 +0800

    first push

---
+++ java/src/main/java/com/derekque/java/App.java
@@ -0,0 +1,13 @@
+package com.derekque.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
